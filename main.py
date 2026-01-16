b009cd0f87e930eea973bc045ac8f1ec7d86cdf7
YunaH <Yunahan53@gmail.com>
2026-01-16 00:39:08 -0500

    Added logic for the ball to change directions or 'bounce' when it collides with the top and bottom walls.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
 import time
 
 # Screen Setup
@@ -9,9 +10,10 @@ screen.bgcolor("black")
 screen.title("The Pong Arcade Game")
 screen.tracer(0)
 
-# Create 2 paddles
+# Create 2 paddles and ball
 r_paddle = Paddle((550, 0))
 l_paddle = Paddle((-550, 0))
+ball = Ball()
 
 screen.listen()
 # Right paddle movement
@@ -22,11 +24,16 @@ screen.onkey(r_paddle.down, "Down")
 screen.onkey(l_paddle.up, "w")
 screen.onkey(l_paddle.down, "s")
 
+
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
+    ball.move()
 
+    # Detects balls collision with the top and bottom wall
+    if ball.ycor() > 330 or ball.ycor() < -330:
+        ball.bounce()
 
 
 screen.exitonclick()
